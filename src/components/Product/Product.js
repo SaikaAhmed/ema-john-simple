71782b939c4521000dde48fc3bb658aa78c6a339
SaikaAhmed <saikaaridii.com>
2020-08-25 12:15:41 +0600

    finally done one project

---
+++ src/components/Product/Product.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const Product = (props) => {
+    // console.log(props);
+
+    const {img, name, seller, price, stock} = props.product;
+    return (
+        <div className="product">
+            <div>
+              <img src={img} alt=""/>
+            </div>
+           
+            <div className="product-name">
+            <h4>{name}</h4>
+            <br/>
+            <p><small>by: {seller} </small></p>
+        <p>${price}</p>
+        <p><small>Only {stock} left in stock - Order soon</small></p>
+          <button onClick={() => props.handleAddProduct(props.product)} className="order-btn"> <FontAwesomeIcon icon={faShoppingCart} />Add to Cart</button>
+            </div>
+    
+        </div>
+    );
+};
+
+export default Product;
